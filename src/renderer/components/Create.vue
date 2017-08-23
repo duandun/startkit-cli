34cfe7b64cabdbdbc37ab465dff52a4088ae863b
changrui <changrui@didichuxing.com>
2017-08-23 20:02:13 +0800

    first commit

---
+++ src/renderer/components/Create.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <div class="create-container">
+    新建项目
+    <Row>
+      <Button type="primary" @click="$router.go(-1)">返回</Button>
+    </Row>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="less">
+  .create-container {
+    padding: 20px;
+    text-align: center;
+  }
+</style>
---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="wrapper">
+    <img id="logo" src="~@/assets/logo.png" alt="electron-vue">
+    <main>
+      <!-- <div class="left-side">
+        <span class="title">
+          Welcome to your new project!
+        </span>
+        <system-information></system-information>
+      </div>
+
+      <div class="right-side">
+        <div class="doc">
+          <div class="title">Getting Started</div>
+          <p>
+            electron-vue comes packed with detailed documentation that covers everything from
+            internal configurations, using the project structure, building your application,
+            and so much more.
+          </p>
+          <button @click="open('https://simulatedgreg.gitbooks.io/electron-vue/content/')">Read the Docs</button><br><br>
+        </div>
+        <div class="doc">
+          <div class="title alt">Other Documentation</div>
+          <button class="alt" @click="open('https://electron.atom.io/docs/')">Electron</button>
+          <button class="alt" @click="open('https://vuejs.org/v2/guide/')">Vue.js</button>
+        </div>
+      </div> -->
+    </main>
+    <Button type="primary" @click="createProj">创建项目</Button>
+  </div>
+</template>
+
+<script>
+  import SystemInformation from './LandingPage/SystemInformation'
+  import Create from './Create'
+
+  export default {
+    name: 'landing-page',
+    components: { SystemInformation, Create },
+    methods: {
+      open (link) {
+        this.$electron.shell.openExternal(link)
+      },
+      createProj () {
+        this.$router.push({
+          name: 'create-project'
+        })
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
